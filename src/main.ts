bb1277a8923920b3916812356736811e209c4c97
edsghot <edsghot@gmail.com>
2024-09-08 16:06:19 -0500

    se agrego el cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule,{
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
